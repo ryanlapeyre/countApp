dae3f62a948b60eda278b88c31372fcee9063247
ryanlapeyre <ryanlapeyre@gmail.com>
2017-09-30 16:41:09 -0500

    keys

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
-  config.api_key = "<api_key>"
-  config.secret = "<secret>"
+  config.api_key = "b545bba2c23888f80b489203bf1bf815"
+  config.secret = "d9750f9a47f0ca3a038c53ca5c89de79"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
   config.after_authenticate_job = false
