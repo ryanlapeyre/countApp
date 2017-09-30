500b14e805bdcbd7cad8318ec866b379817def01
ryanlapeyre <ryanlapeyre@gmail.com>
2017-09-30 15:56:25 -0500

    removed config uri

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -3,7 +3,6 @@ ShopifyApp.configure do |config|
   config.api_key = "<api_key>"
   config.secret = "<secret>"
   config.scope = "read_orders, read_products"
-  config.redirect_uri = "https://countshopapp.herokuapp.com/auth/shopify/callback"
   config.embedded_app = true
   config.after_authenticate_job = false
   config.session_repository = Shop
